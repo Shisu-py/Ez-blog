e76ca6dae5224d6c0765066aae4f5717b9edd041
Shisu-py <rengservant@gmail.com>
2021-08-11 22:13:50 +0300

    Views and Mixin

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
 
 from .models import Post, Tag
+from .utils import objectDetailMixin
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context = {'posts': posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+
+class PostDetail(objectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug_iexact = slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+class TagDetail(objectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+    
