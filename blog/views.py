f614a8457b472c23e68cbf7857e9d6078f351d76
Shisu-py <rengservant@gmail.com>
2021-09-17 17:34:01 +0300

    delete_form

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
+from django.http import request
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
@@ -41,3 +43,14 @@ class TagUpdate(ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
+
+
+class TagDelete(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+
+    def post(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        tag.delete()
+        return redirect(reverse('tags_list_url'))
